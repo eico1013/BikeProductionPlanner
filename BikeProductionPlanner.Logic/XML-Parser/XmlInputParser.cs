b7b74a445818e25f69126685ceee45e4086a43ae
Constantin <eico1013@hs-karlsruhe.de>
2018-04-15 14:08:56 +0200

    :sparkles: add XML Parser

---
+++ BikeProductionPlanner.Logic/XML-Parser/XmlInputParser.cs
@@ -0,0 +1,296 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Xml;
+using BikeProductionPlanner.Logic.Database;
+
+namespace BikeProductionPlanner.Logic
+{
+    public sealed class XmlInputParser
+    {
+        public List<WarehouseStock> WarehouseStocks { get; private set; }
+        public List<FutureInwardStockMovment> FutureInwardStockMovments { get; private set; }
+        public List<WaitingListWorkstation> WaitingListWorkstations { get; private set; }
+        public List<WaitingListStock> WaitingListStocks { get; private set; }
+        public List<OrderInWork> OrdersInWork { get; private set; }
+
+        private static readonly XmlInputParser instance = new XmlInputParser();
+        private static readonly String WAREHOUSE_STOCK = "warehousestock";
+        private static readonly String FUTURE_INWARD_STOCKMOVEMENT = "futureinwardstockmovement";
+
+        private static readonly String WAITINGLIST_WORKSATION = "waitinglistworkstations";
+        private static readonly String WAITINGLIST_STOCK = "waitingliststock";
+        private static readonly String ORDER_IN_WORK = "ordersinwork";
+
+        private XmlInputParser()
+        {
+            WarehouseStocks = new List<WarehouseStock>();
+            FutureInwardStockMovments = new List<FutureInwardStockMovment>();
+            WaitingListWorkstations = new List<WaitingListWorkstation>();
+            WaitingListStocks = new List<WaitingListStock>();
+            OrdersInWork = new List<OrderInWork>();
+        }
+
+        public static XmlInputParser Instance
+        {
+            get
+            {
+                return instance;
+            }
+        }
+
+        //public int getAmountFromWareHouseStockId(int Id)
+        //{
+        //    foreach (var item in WarehouseStocks)
+        //    {
+        //        if (item.Id == Id)
+        //        {
+        //            return item.Amount;
+        //        }
+        //    }
+        //    return 0;
+        //}
+
+        private void clearLists()
+        {
+            WarehouseStocks.Clear();
+            FutureInwardStockMovments.Clear();
+            WaitingListWorkstations.Clear();
+            WaitingListStocks.Clear();
+            OrdersInWork.Clear();
+        }
+
+        public bool ParseXml(String XmlFile)
+        {
+            clearLists();
+
+            if (XmlFile == null || !File.Exists(XmlFile) || !Path.GetExtension(XmlFile).ToUpper().Equals(".XML"))
+            {
+
+                return false;
+            }
+
+            String tag = "";
+            XmlTextReader reader = new XmlTextReader(XmlFile);
+
+            while (reader.Read())
+            {
+                if (reader.NodeType == XmlNodeType.Element &&
+                    ((reader.Name).Equals(WAREHOUSE_STOCK)
+                        || (reader.Name).Equals(FUTURE_INWARD_STOCKMOVEMENT)
+                        || (reader.Name).Equals(WAITINGLIST_WORKSATION)
+                        || (reader.Name).Equals(WAITINGLIST_STOCK)
+                        || (reader.Name).Equals(ORDER_IN_WORK)))
+                {
+                    tag = reader.Name;
+                }
+                else if (reader.NodeType == XmlNodeType.Element && (reader.Name).Equals("article") && tag == WAREHOUSE_STOCK)
+                {
+                    reader = getWarehouseStockValue(reader);
+                }
+                else if (reader.NodeType == XmlNodeType.Element && (reader.Name).Equals("order") && tag == FUTURE_INWARD_STOCKMOVEMENT)
+                {
+                    reader = getFutureInwardStockMovment(reader);
+                }
+                else if (reader.NodeType == XmlNodeType.Element && (reader.Name).Equals("workplace") && tag == WAITINGLIST_WORKSATION)
+                {
+                    reader = getWaitinglistworkstation(reader);
+                }
+                else if (reader.NodeType == XmlNodeType.Element && (reader.Name).Equals("waitinglist") && tag == WAITINGLIST_WORKSATION)
+                {
+                    reader = addWaitingListToWaitinglistWorkstation(reader);
+                }
+                else if (reader.NodeType == XmlNodeType.Element && (reader.Name).Equals("missingpart") && tag == WAITINGLIST_STOCK)
+                {
+                    reader = getWaitingListStock(reader);
+                }
+                else if (reader.NodeType == XmlNodeType.Element && (reader.Name).Equals("waitinglist") && tag == WAITINGLIST_STOCK)
+                {
+                    reader = addWaitingListToWaitingListStock(reader);
+                }
+                else if (reader.NodeType == XmlNodeType.Element && (reader.Name).Equals("workplace") && tag == ORDER_IN_WORK)
+                {
+                    reader = getOrderInWork(reader);
+                }
+            } 
+            return true;
+        }
+
+        private XmlTextReader getWarehouseStockValue(XmlTextReader reader)
+        {
+            WarehouseStock newWarehousestock = new WarehouseStock();
+            while (reader.MoveToNextAttribute())
+            {
+                switch (reader.Name)
+                {
+                    case "id":
+                        newWarehousestock.Id = Int32.Parse(reader.Value);
+                        break;
+                    case "amount":
+                        newWarehousestock.Amount = Int32.Parse(reader.Value);
+                        break;
+                    case "price":
+                        newWarehousestock.Price = Double.Parse(reader.Value);
+                        break;
+                    case "stockvalue":
+                        newWarehousestock.StockValue = Double.Parse(reader.Value);
+                        break;
+                    case "startamount":
+                        newWarehousestock.StartAmount = Int32.Parse(reader.Value);
+                        break;
+                    case "pct":
+                        newWarehousestock.PCT = Double.Parse(reader.Value);
+                        break;
+                }
+            }
+
+            this.WarehouseStocks.Add(newWarehousestock);
+            return reader;
+        }
+
+        private XmlTextReader getFutureInwardStockMovment(XmlTextReader reader)
+        {
+            FutureInwardStockMovment newFutureInwardStockMovment = new FutureInwardStockMovment();
+            while (reader.MoveToNextAttribute())
+            {
+                switch (reader.Name)
+                {
+                    case "orderperiod":
+                        newFutureInwardStockMovment.OrderPeriod = Int32.Parse(reader.Value);
+                        break;
+                    case "id":
+                        newFutureInwardStockMovment.Id = Int32.Parse(reader.Value);
+                        break;
+                    case "mode":
+                        newFutureInwardStockMovment.Mode = Int32.Parse(reader.Value);
+                        break;
+                    case "article":
+                        newFutureInwardStockMovment.Article = Int32.Parse(reader.Value);
+                        break;
+                    case "amount":
+                        newFutureInwardStockMovment.Amount = Int32.Parse(reader.Value);
+                        break;
+
+                }
+            }
+
+            this.FutureInwardStockMovments.Add(newFutureInwardStockMovment);
+            return reader;
+        }
+
+        private XmlTextReader getWaitinglistworkstation(XmlTextReader reader)
+        {
+            WaitingListWorkstation newWaitingListWorksation = new WaitingListWorkstation();
+            while (reader.MoveToNextAttribute())
+            {
+                switch (reader.Name)
+                {
+                    case "id":
+                        newWaitingListWorksation.Id = Int32.Parse(reader.Value);
+                        break;
+                    case "amount":
+                        newWaitingListWorksation.TimeNeed = Int32.Parse(reader.Value);
+                        break;
+                }
+            }
+
+            this.WaitingListWorkstations.Add(newWaitingListWorksation);
+            return reader;
+        }
+
+        private XmlTextReader addWaitingListToWaitinglistWorkstation(XmlTextReader reader)
+        {
+            WaitingList newWaitingList = new WaitingList();
+            while (reader.MoveToNextAttribute())
+            {
+                switch (reader.Name)
+                {
+                    case "order":
+                        newWaitingList.Order = Int32.Parse(reader.Value);
+                        break;
+                    case "item":
+                        newWaitingList.Item = Int32.Parse(reader.Value);
+                        break;
+                    case "amount":
+                        newWaitingList.Amount = Int32.Parse(reader.Value);
+                        break;
+                    case "timeneed":
+                        newWaitingList.TimeNeed = Int32.Parse(reader.Value);
+                        break;
+                }
+            }
+
+            this.WaitingListWorkstations[WaitingListWorkstations.Count - 1].WaitingList.Add(newWaitingList);
+            return reader;
+        }
+
+        private XmlTextReader getWaitingListStock(XmlTextReader reader)
+        {
+            WaitingListStock newWaitingListStock = new WaitingListStock();
+            while (reader.MoveToNextAttribute())
+            {
+                switch (reader.Name)
+                {
+                    case "id":
+                        newWaitingListStock.Id = Int32.Parse(reader.Value);
+                        break;
+                }
+            }
+
+            this.WaitingListStocks.Add(newWaitingListStock);
+            return reader;
+        }
+
+        private XmlTextReader addWaitingListToWaitingListStock(XmlTextReader reader)
+        {
+            WaitingList newWaitingList = new WaitingList();
+            while (reader.MoveToNextAttribute())
+            {
+                switch (reader.Name)
+                {
+                    case "order":
+                        newWaitingList.Order = Int32.Parse(reader.Value);
+                        break;
+                    case "item":
+                        newWaitingList.Item = Int32.Parse(reader.Value);
+                        break;
+                    case "amount":
+                        newWaitingList.Amount = Int32.Parse(reader.Value);
+                        break;
+                }
+            }
+
+            this.WaitingListStocks[WaitingListStocks.Count - 1].WaitingList.Add(newWaitingList);
+            return reader;
+        }
+
+        private XmlTextReader getOrderInWork(XmlTextReader reader)
+        {
+            OrderInWork newOrderInWork = new OrderInWork();
+            while (reader.MoveToNextAttribute())
+            {
+                switch (reader.Name)
+                {
+                    case "id":
+                        newOrderInWork.Id = Int32.Parse(reader.Value);
+                        break;
+                    case "order":
+                        newOrderInWork.Order = Int32.Parse(reader.Value);
+                        break;
+                    case "item":
+                        newOrderInWork.Item = Int32.Parse(reader.Value);
+                        break;
+                    case "amount":
+                        newOrderInWork.Amount = Int32.Parse(reader.Value);
+                        break;
+                    case "timeneed":
+                        newOrderInWork.TimeNeed = Int32.Parse(reader.Value);
+                        break;
+                }
+            }
+
+            this.OrdersInWork.Add(newOrderInWork);
+            return reader;
+        }
+    }
+}
---
+++ BikeProductionPlanner.Logic/XML-Parser/XmlOutputParser.cs
@@ -0,0 +1,239 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Xml;
+using BikeProductionPlanner.Logic.Database;
+
+namespace BikeProductionPlanner.Logic
+{
+    public sealed class XmlOutputParser
+    {
+        public List<SellWish> SellWish { get; set; }
+        public List<SellDirect> SellDirect { get; set; }
+        public List<OrderList> OrderList { get; set; }
+        public List<ProductionList> ProductionList { get; set; }
+        public List<WorkingItemList> WorkingItemList { get; set; }
+        public Boolean ProductioListSorted { get; set; }
+
+        private static readonly XmlOutputParser instance = new XmlOutputParser();
+
+        private XmlOutputParser()
+        {
+            SellWish = new List<SellWish>();
+            SellDirect = new List<SellDirect>();
+            OrderList = new List<OrderList>();
+            ProductionList = new List<ProductionList>();
+            WorkingItemList = new List<WorkingItemList>();
+            ProductioListSorted = false;
+        }
+
+        public static XmlOutputParser Instance
+        {
+            get
+            {
+                return instance;
+            }
+        }
+
+        public bool createOutputXml(String XmlFile)
+        {
+            if (XmlFile == null || XmlFile == "")
+            {
+                //throw new ArgumentException("XmlFile");
+                return false;
+            }
+
+            XmlDocument doc = new XmlDocument();
+            XmlNode myRoot;
+
+            myRoot = doc.CreateElement("input");
+            doc.AppendChild(myRoot);
+
+            myRoot.AppendChild(getQualitycontrolNode(doc));
+            myRoot.AppendChild(getSellwishNode(doc));
+            myRoot.AppendChild(getSellDirectNode(doc));
+            myRoot.AppendChild(getOrderListNode(doc));
+            myRoot.AppendChild(getProductionListNode(doc));
+            myRoot.AppendChild(getWorkingItemListNode(doc));
+
+            doc.Save(XmlFile);
+
+            return true;
+        }
+
+        public ProductionList getProductionItemById(int article)
+        {
+            foreach (var item in ProductionList)
+            {
+                if(item.Article == article)
+                {
+                    return item;
+                }
+            }
+            return null;
+        }
+
+        private XmlNode getQualitycontrolNode(XmlDocument doc)
+        {
+            XmlNode myNode;
+            XmlAttribute myAttribute;
+
+            myNode = doc.CreateElement("qualitycontrol");
+
+            myAttribute = doc.CreateAttribute("delay");
+            myAttribute.InnerText = "0";
+            myNode.Attributes.Append(myAttribute);
+
+            myAttribute = doc.CreateAttribute("losequantity");
+            myAttribute.InnerText = "0";
+            myNode.Attributes.Append(myAttribute);
+
+            myAttribute = doc.CreateAttribute("type");
+            myAttribute.InnerText = "no";
+
+            myNode.Attributes.Append(myAttribute);
+            return myNode;
+        }
+
+        private XmlNode getSellwishNode(XmlDocument doc)
+        {
+            XmlNode myNode, myChildNode;
+            XmlAttribute myAttribute;
+
+            myNode = doc.CreateElement("sellwish");
+
+            foreach (var sw in this.SellWish)
+            {
+                myChildNode = doc.CreateElement("item");
+
+                myAttribute = doc.CreateAttribute("quantity");
+                myAttribute.InnerText = sw.Quantity.ToString();
+                myChildNode.Attributes.Append(myAttribute);
+
+                myAttribute = doc.CreateAttribute("article");
+                myAttribute.InnerText = sw.Article.ToString();
+                myChildNode.Attributes.Append(myAttribute);
+
+                myNode.AppendChild(myChildNode);
+            }
+
+            return myNode;
+        }
+
+        private XmlNode getSellDirectNode(XmlDocument doc)
+        {
+            XmlNode myNode, myChildNode;
+            XmlAttribute myAttribute;
+
+            myNode = doc.CreateElement("selldirect");
+
+            foreach (var sd in this.SellDirect)
+            {
+                myChildNode = doc.CreateElement("item");
+
+                myAttribute = doc.CreateAttribute("quantity");
+                myAttribute.InnerText = sd.Quantity.ToString();
+                myChildNode.Attributes.Append(myAttribute);
+
+                myAttribute = doc.CreateAttribute("article");
+                myAttribute.InnerText = sd.Article.ToString();
+                myChildNode.Attributes.Append(myAttribute);
+
+                myAttribute = doc.CreateAttribute("penalty");
+                myAttribute.InnerText = sd.Penalty.ToString();
+                myChildNode.Attributes.Append(myAttribute);
+
+                myAttribute = doc.CreateAttribute("price");
+                myAttribute.InnerText = sd.Price.ToString();
+                myChildNode.Attributes.Append(myAttribute);
+
+                myNode.AppendChild(myChildNode);
+            }
+
+            return myNode;
+        }
+
+        private XmlNode getOrderListNode(XmlDocument doc)
+        {
+            XmlNode myNode, myChildNode;
+            XmlAttribute myAttribute;
+
+            myNode = doc.CreateElement("orderlist");
+
+            foreach (var ol in this.OrderList)
+            {
+                myChildNode = doc.CreateElement("order");
+
+                myAttribute = doc.CreateAttribute("quantity");
+                myAttribute.InnerText = ol.Quantity.ToString();
+                myChildNode.Attributes.Append(myAttribute);
+
+                myAttribute = doc.CreateAttribute("article");
+                myAttribute.InnerText = ol.Article.ToString();
+                myChildNode.Attributes.Append(myAttribute);
+
+                myAttribute = doc.CreateAttribute("modus");
+                myAttribute.InnerText = ol.Modus.ToString();
+                myChildNode.Attributes.Append(myAttribute);
+
+                myNode.AppendChild(myChildNode);
+            }
+
+            return myNode;
+        }
+
+        private XmlNode getProductionListNode(XmlDocument doc)
+        {
+            XmlNode myNode, myChildNode;
+            XmlAttribute myAttribute;
+
+            myNode = doc.CreateElement("productionlist");
+
+            foreach (var pl in this.ProductionList)
+            {
+                myChildNode = doc.CreateElement("production");
+
+                myAttribute = doc.CreateAttribute("quantity");
+                myAttribute.InnerText = pl.Quantity.ToString();
+                myChildNode.Attributes.Append(myAttribute);
+
+                myAttribute = doc.CreateAttribute("article");
+                myAttribute.InnerText = pl.Article.ToString();
+                myChildNode.Attributes.Append(myAttribute);
+
+                myNode.AppendChild(myChildNode);
+            }
+
+            return myNode;
+        }
+
+        private XmlNode getWorkingItemListNode(XmlDocument doc)
+        {
+            XmlNode myNode, myChildNode;
+            XmlAttribute myAttribute;
+
+            myNode = doc.CreateElement("workingtimelist");
+
+            foreach (var wt in this.WorkingItemList)
+            {
+                myChildNode = doc.CreateElement("workingtime");
+
+                myAttribute = doc.CreateAttribute("overtime");
+                myAttribute.InnerText = wt.WorkingOvertime.ToString();
+                myChildNode.Attributes.Append(myAttribute);
+
+                myAttribute = doc.CreateAttribute("shift");
+                myAttribute.InnerText = wt.Shift.ToString();
+                myChildNode.Attributes.Append(myAttribute);
+
+                myAttribute = doc.CreateAttribute("station");
+                myAttribute.InnerText = wt.Station.ToString();
+                myChildNode.Attributes.Append(myAttribute);
+
+                myNode.AppendChild(myChildNode);
+            }
+
+            return myNode;
+        }
+
+    }
+}
