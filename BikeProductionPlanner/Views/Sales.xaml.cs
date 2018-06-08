dc27380a00bdf86c00ca0f47dc8b80abba274ef2
Unknown <ra-schueler@gmx.de>
2018-06-08 17:12:00 +0200

    :sparkles: Funktionsintegration Sales

--- BikeProductionPlanner/Views/Sales.xaml.cs
+++ BikeProductionPlanner/Views/Sales.xaml.cs
@@ -124,7 +124,7 @@ namespace BikeProductionPlanner.Views
 
             try
             {
-
+                //Vertrieb
                 StorageService.Instance.AddForecast(0, new ForecastPeriod(Convert.ToInt32(period0product1.Text), Convert.ToInt32(period0product2.Text), Convert.ToInt32(period0product3.Text)));
                 StorageService.Instance.AddForecast(1, new ForecastPeriod(Convert.ToInt32(period1product1.Text), Convert.ToInt32(period1product2.Text), Convert.ToInt32(period1product3.Text)));
                 StorageService.Instance.AddForecast(2, new ForecastPeriod(Convert.ToInt32(period2product1.Text), Convert.ToInt32(period2product2.Text), Convert.ToInt32(period2product3.Text)));
@@ -146,7 +146,20 @@ namespace BikeProductionPlanner.Views
                 StorageService.Instance.prognose3P2 = Convert.ToInt32(period3product2.Text);
                 StorageService.Instance.prognose3P3 = Convert.ToInt32(period3product3.Text);
 
-                Fortfahren_Button.Visibility = Visibility.Visible;
+                //Direktvertrieb
+                StorageService.Instance.AddSellWish(new SellWish(StorageService.Instance.GetForecastForPeriod(0).Product1, 1));
+                StorageService.Instance.AddSellWish(new SellWish(StorageService.Instance.GetForecastForPeriod(0).Product2, 2));
+                StorageService.Instance.AddSellWish(new SellWish(StorageService.Instance.GetForecastForPeriod(0).Product3, 3));
+
+                StorageService.Instance.AddSellDirect(new SellDirect(Convert.ToInt32(directProduct1.Text), 1, Convert.ToInt32(contractPenaltyProduct1.Text), Convert.ToInt32(retailPriceProduct1.Text)));
+                StorageService.Instance.AddSellDirect(new SellDirect(Convert.ToInt32(directProduct2.Text), 2, Convert.ToInt32(contractPenaltyProduct2.Text), Convert.ToInt32(retailPriceProduct2.Text)));
+                StorageService.Instance.AddSellDirect(new SellDirect(Convert.ToInt32(directProduct3.Text), 3, Convert.ToInt32(contractPenaltyProduct3.Text), Convert.ToInt32(retailPriceProduct3.Text)));
+
+                StorageService.Instance.vertriebswunschP1 = StorageService.Instance.vertriebswunschP1 + Convert.ToInt32(directProduct1.Text);
+                StorageService.Instance.vertriebswunschP2 = StorageService.Instance.vertriebswunschP2 + Convert.ToInt32(directProduct2.Text);
+                StorageService.Instance.vertriebswunschP3 = StorageService.Instance.vertriebswunschP3 + Convert.ToInt32(directProduct3.Text);
+
+
             }
             catch
             {
@@ -205,6 +218,10 @@ namespace BikeProductionPlanner.Views
                 MessageBox.Show("Fehler: Alle Felder müssen ausgefüllt werden.");
                 return;
             }
+
+            MainWindowFinal.Instance.NavigateTo(Logic.UI.MenuItems.MenuItemsEnum.SafetyStock);
+            ListView lvMenu = (ListView)MainWindowFinal.Instance.FindName("ListViewMenu");
+            lvMenu.SelectedIndex = 3;
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
@@ -213,6 +230,30 @@ namespace BikeProductionPlanner.Views
             ListView lvMenu = (ListView)MainWindowFinal.Instance.FindName("ListViewMenu");
             lvMenu.SelectedIndex = 3;
         }
+
+        private void Forecast1_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            try
+            {
+                Int32 directsum1 = 0;
+
+                if (directProduct1 != null)
+                    directsum1 += Convert.ToInt32(directProduct1.Text);
+                if (directProduct2 != null)
+                    directsum1 += Convert.ToInt32(directProduct2.Text);
+                if (directProduct3 != null)
+                    directsum1 += Convert.ToInt32(directProduct3.Text);
+
+                if (directSum != null)
+                    directSum.Text = directsum1.ToString();
+            }
+            catch
+            {
+
+            }
+        }
+
+        
     }
 
  }
